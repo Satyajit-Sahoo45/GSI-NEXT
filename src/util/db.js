9daa2e597b35348ffe5005b23d34ab78298c1070
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-08-30 11:44:52 +0530

    change firebase config connection

--- src/util/db.js
+++ src/util/db.js
@@ -3,19 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCS1H8GgHeXxprx2yro5dIxFA0O_0wyl70",
-    authDomain: "loginsignupgsi.firebaseapp.com",
-    projectId: "loginsignupgsi",
-    storageBucket: "loginsignupgsi.appspot.com",
-    messagingSenderId: "70847031831",
-    appId: "1:70847031831:web:163cb409e13163040a0a52"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBPZomDcmoh-OCV-wTAGDJlAll49iONBu4",
-//     authDomain: "reviewrating-27cff.firebaseapp.com",
-//     projectId: "reviewrating-27cff",
-// };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);

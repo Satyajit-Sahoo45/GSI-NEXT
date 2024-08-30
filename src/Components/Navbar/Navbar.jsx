d885739ec923686fdbc27b073a8ec30dd283f21f
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-08-30 11:52:14 +0530

    update navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,6 +30,7 @@ const Navbar = () => {
                 <li><Link className="block lg:inline-block text-sm text-gray-400 hover:text-gray-500 px-4 py-2 lg:px-0" to="/invoice">Invoice</Link></li>
                 <li><Link className="block lg:inline-block text-sm text-gray-400 hover:text-gray-500 px-4 py-2 lg:px-0" to="/ReviewandRating">Review&Rating</Link></li>
                 <li><Link className="block lg:inline-block text-sm text-gray-400 hover:text-gray-500 px-4 py-2 lg:px-0" to="/quickbook">QuickBook</Link></li>
+                <li className='md:hidden block'><Link className="block lg:inline-block text-sm text-gray-400 hover:text-gray-500 px-4 py-2 lg:px-0" to="/profile">Profile</Link></li>
             </ul>
 
             {!loading && (
